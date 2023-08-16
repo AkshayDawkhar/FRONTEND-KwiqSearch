10904f773b88fcff4fa92f6c568d29cc5af1153e
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-08-16 20:26:54 +0530

    change image UI

--- lib/controller/image.dart
+++ lib/controller/image.dart
@@ -8,17 +8,18 @@ import '../constants.dart';
 
 class ImageController extends GetxController {
   ImageController({required this.projectID});
+
   int projectID;
 
   Future<int> uploadImage(File image) async {
-    Map m = {"name": 'akshay','project_id': projectID};
+    Map m = {"name": 'akshay', 'project_id': projectID};
     final url = Uri.parse('$HOSTNAME/home/images/');
     // final response = await http.post(url,
     //     headers: <String, String>{
     //       'Content-Type': 'application/json; charset=UTF-8',
     //     },
     //     body: jsonEncode(m));
-    var request = http.MultipartRequest('POST',url);
+    var request = http.MultipartRequest('POST', url);
     request.fields['project_id'] = projectID.toString();
     request.fields['name'] = 'name';
     request.files.add(
@@ -42,9 +43,10 @@ class ImageController extends GetxController {
     // }
     update();
   }
-Future<int> uploadUnitImage(File image,int unitID) async {
+
+  Future<int> uploadUnitImage(File image, int unitID) async {
     final url = Uri.parse('$HOSTNAME/home/FloorMaps/');
-    var request = http.MultipartRequest('POST',url);
+    var request = http.MultipartRequest('POST', url);
     request.fields['unit'] = unitID.toString();
     request.fields['name'] = 'name';
     request.files.add(
@@ -58,7 +60,22 @@ Future<int> uploadUnitImage(File image,int unitID) async {
       print(value);
     });
     print(response.statusCode);
-   return response.statusCode;
+    return response.statusCode;
+    // if (response.statusCode == 201) {
+    //   Get.back();
+    //   Get.defaultDialog(title: 'Successful', content: const Text('Created successfully'), backgroundColor: Colors.greenAccent,textCancel: 'OK');
+    // } else {
+    //   // Get.defaultDialog(title: 'Error', content: const Text('Something Went wrong'), backgroundColor: Colors.redAccent);
+    //   getErrorDialog(response.body);
+    // }
+    update();
+  }
+
+  Future<int> deleteUnitImage(int imageID) async {
+    final url = Uri.parse('$HOSTNAME/home/FloorMap/$imageID');
+    http.Response response = await http.delete(url);
+    print(response.statusCode);
+    return response.statusCode;
     // if (response.statusCode == 201) {
     //   Get.back();
     //   Get.defaultDialog(title: 'Successful', content: const Text('Created successfully'), backgroundColor: Colors.greenAccent,textCancel: 'OK');
--- lib/views/project/image.dart
+++ lib/views/project/image.dart
@@ -59,8 +59,8 @@ class _ImagePageState extends State<ImagePage> {
                         ElevatedButton(
                           onPressed: () {
                             // Handle edit button press
-                          // imagePickerDialog();
-                        uploadProjectImage(89);
+                            // imagePickerDialog();
+                            uploadProjectImage(89);
                           },
                           child: Icon(Icons.edit),
                         ),
@@ -170,7 +170,7 @@ class _ImagePageState extends State<ImagePage> {
     }
   }
 
-  Future<File?> imagePickerDialog() async{
+  Future<File?> imagePickerDialog() async {
     var pickedImage;
     await Get.dialog(AlertDialog(
       title: Text('Change Image'),
@@ -178,8 +178,8 @@ class _ImagePageState extends State<ImagePage> {
         IconButton(
             onPressed: () async {
               pickedImage = pickImage(ImageSource.camera);
-            Get.back();
-              },
+              Get.back();
+            },
             icon: Icon(Icons.camera)),
         IconButton(
             onPressed: () {
@@ -202,80 +202,81 @@ class _ImagePageState extends State<ImagePage> {
   }
 
   Widget unitContainer(Unit unit, int index) {
-    String imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Sample_Floorplan.jpg';
-    if(unit.floorMap.firstOrNull != null) {
-      imageUrl = HOSTNAME + unit.floorMap.firstOrNull!.image;
-    }
     return Container(
-        margin: EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          border: Border.all(width: 2),
-          color: Colors.blue[200],
-        ),
-        child: InkWell(
-          onTap: () {
-            print(unit.floorMap.firstOrNull!.image);
-            Get.dialog(
-              InteractiveViewer(
-                clipBehavior: Clip.none,
-                maxScale: 90,
-                child: Image.network(imageUrl),
-              ),
-            );
-            // interestedController.switchUnit(index, unit.id);
-          },
-          child: Column(
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(width: 2),
+        color: Colors.blue[200],
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(' ${unitToName(unit.unit)} | ${unit.carpetArea} sqft | ${numberToLCr(unit.price.toDouble())}'),
-                IconButton(onPressed: (){
-                  uploadUnitImage(unit);
-
-                }, color: Colors.black,icon: Icon(Icons.add_photo_alternate))
-                ],
-              ),
-              Stack(
-                alignment: Alignment.bottomRight, // Align items to the right bottom corner
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.network(
-                      imageUrl,
-                      width: double.infinity,
-                      // height: 200, // Adjust the height as needed
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 1, // Adjust the position as needed
-                    right: 1, // Adjust the position as needed
-                    child: Row(
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            // Handle edit button press
-                          },
-                          child: Icon(Icons.edit),
-                        ),
-                        SizedBox(width: 10),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(foregroundColor: Colors.red),
-                          child: Icon(Icons.delete),
-                          onPressed: () {
-                            // Handle delete button press
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+              Text(' ${unitToName(unit.unit)} | ${unit.carpetArea} sqft | ${numberToLCr(unit.price.toDouble())}'),
+              IconButton(
+                  onPressed: () {
+                    uploadUnitImage(unit);
+                  },
+                  color: Colors.black,
+                  icon: Icon(Icons.add_photo_alternate))
             ],
           ),
-        ),
-      );
+          unit.floorMap.isNotEmpty
+              ? Stack(
+                  alignment: Alignment.bottomRight, // Align items to the right bottom corner
+                  children: [
+                    InkWell(
+                      onTap: (){
+
+                        print(unit.floorMap.firstOrNull!.image);
+                        Get.dialog(
+                          InteractiveViewer(
+                            clipBehavior: Clip.none,
+                            maxScale: 90,
+                            child: Image.network('$HOSTNAME${unit.floorMap.first.image}'),
+                          ),
+                        );
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.network(
+                          '$HOSTNAME${unit.floorMap.first.image}',
+                          width: double.infinity,
+                          // height: 200, // Adjust the height as needed
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 1, // Adjust the position as needed
+                      right: 1, // Adjust the position as needed
+                      child: Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              // Handle edit button press
+                            },
+                            child: Icon(Icons.edit),
+                          ),
+                          SizedBox(width: 10),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(foregroundColor: Colors.red),
+                            child: Icon(Icons.delete),
+                            onPressed: () {
+                              // imageController.deleteUnitImage();
+                              // Handle delete button press
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              : Container(),
+        ],
+      ),
+    );
   }
 }
